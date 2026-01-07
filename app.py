2526bd8478a378de97b5ca8bf8d1b8c7f204e618
root <root@LAPTOP-7U5TPNR2.localdomain>
2026-01-06 22:28:14 -0300

    implemented login, still need to fix bug

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+connection = sqlite3.connect('database.db')
+
+cursor = connection.cursor()
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        cursor.execute("INSERT INTO user(username TXT UNIQUE, password TXT) VALUES(?, ?)", username, password)
+        return render_template("index.html")        
